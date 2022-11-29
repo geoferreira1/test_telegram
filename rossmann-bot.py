1e0e88146d28598f9f5e8f301f38a96f86a229b1
geoferreira1 <geovanaferreira47@gmail.com>
2022-11-28 22:55:23 -0300

    update commit

--- rossmann-bot.py
+++ rossmann-bot.py
@@ -30,8 +30,8 @@ def send_message(chat_id, text):
 
 def load_dataset(store_id):
     # loading test dataset
-    df10 = pd.read_csv( '/home/geo/COMUNIDADE_DS/DS_EM_PRODUCAO/Data/test.csv' )
-    df_store_raw = pd.read_csv( '/home/geo/COMUNIDADE_DS/DS_EM_PRODUCAO/Data/store.csv' )
+    df10 = pd.read_csv( 'test.csv' )
+    df_store_raw = pd.read_csv( 'store.csv' )
 
     # merge test dataset + store
     df_test = pd.merge( df10, df_store_raw, how='left', on='Store' )
